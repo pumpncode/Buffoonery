1071486276f5187690556a09840ef2ab7360e8be
pinkmaggit-hub <pinkmaggitmtg@gmail.com>
2025-01-19 14:06:22 -0300

    repo polish

--- Buffoonery.lua
+++ Buffoonery.lua
@@ -160,38 +160,4 @@ SMODS.Sound({key = 'explosion', path = 'explosion.ogg'})
 SMODS.Sound({key = 'roul1', path = 'roul1.ogg'})
 SMODS.Sound({key = 'roul2', path = 'roul2.ogg'})
 
-
------------------------------------- CHANGELOG (KINDA) ------------------------------------
-
--- 0.1) INITIAL PRIVATE BUILD WITH 2 WORKING JOKERS
--- 0.2) FIRST PUBLIC RELEASE WITH 5 WORKING JOKERS
---		0.2.2 - Updated README.md to better fit joker descriptions,
---       	  - Added a flag to prevent Korny Joker from respawning
---       	  - Added memorized card counter and last card memorized info to Memory Card's description
---			  - Added Xchips and Emult functionality
---		0.2.3 - updated Memory Card's art (made the 8 higher res)
---			  - Made Korny Joker and Memory Card's descriptions less verbose
--- 0.5) FIRST MAJOR UPDATE WITH 3 JOKERS, MEMORY CARD UPDATE, ARTWORK TWEAKS, DESCRIPTION TWEAKS, AND ARTWORK CREDITS
--- 		0.5.1 - Minor tweaks to some of the artwork to better fit Balatro's artstyle
---				Changed Five Fingers from uncommon ($6) to common ($4) 
---		0.5.2 - Added modded suit compatibility to Memory Card
--- 0.6) MAJOR UPDATE WITH TWO NEW JOKERS. UPDATE IS PRIVATE FOR PLAYTESTING PURPOSES. INTERNAL CHANGES TO 1.0 GOALS AND VERSION NUMBEIRING.
--- 		0.6.1 - Fixed Laidback Joker being able to be purchased when player had less than 2 free slots
---		0.6.2 - Added Talisman compat to Cashout Voucher
--- 0.8) BIGGEST UPDATE YET WITH 3 NEW JOKERS (5 TOTAL WITH 0.6'S). ARTWORK FIXES, BALANCE CHANGES, UPDATED 1.0 GOALS, STARTED WORK ON NEW ENHANCEMENT.
---		0.8.1 - Added Brazilian Portuguese localization
---              Fixed Maggit crashing the game when creating a joker
---				Gave the shading some more work on Jebediah Kerman's artwork
--- 0.9) PRE-1.0 UPDATE FEATURING EVERYTHING PLANNED FOR 1.0 EXCEPT THE LAST TWO JOKERS.
--- 1.0) Final content update for this mod. 2 new jokers, aesthetic fixes, balance changes, made pt_BR loc more thorough. SIKE, I'M ADDING MOAR STUFF LETS GOOOOOOOOOOOOOOOOOOO
--- 		1.0.1 - Artwork and localization polish
--- 			  - Five Fingers costs 5$
---			  - Finally made patronizing joker have its initial planned effect, similar to the Cerulean Bell boss blind
-
---- GOALS FOR 1.0.0 (in order of priority, descending) ---
--- 1) Have a whole page (15) of fully implemented and working jokers (D O N E)
-	-- 1.1) Reserve 5 of 15 jokers for Nu Metal \m/  (D O N E)
--- 3) Add 2 decks to the game with sleeve compat (D O N E)
--- 4) Have locs for at least en-us and pt-br (D O N E)
-
--- [Enhancement idea scrapped. Should be used in a new mod in the future] ALSO SIKE, WE HAVE SMODS.ENHANCEMENT NOW LETS GOOOOOOOOOOOOOOOOOOOOOO
+------ CHANGELOG MOVED TO SEPARATE .md FILE ------
