8e14a9942aef5e3dd6212db49248fcf514ca4d2e
Pink Maggit <pinkmaggitmtg@gmail.com>
2025-02-12 12:55:51 -0300

    Clay Shooting fix.lua

--- data/jokers/clays.lua
+++ data/jokers/clays.lua
@@ -14,7 +14,7 @@ SMODS.Joker {
     perishable_compat = true,
     blueprint_compat = false,
     config = {
-        extra = { gold = 8, aim = 0, success = false, once = 0 },
+        extra = { gold = 8, aim = 0, success = false, once = 0, add_check = true },
     },
     loc_txt = {set = 'Joker', key = 'j_buf_clays'},
     loc_vars = function(self, info_queue, card)
@@ -30,21 +30,32 @@ SMODS.Joker {
 		end 
     end,
 	
+	add_to_deck = function(self, card, context)
+		if card.ability.extra.once == 0 then
+			card.ability.extra.once = 1 
+			card.ability.extra.aim = math.random(1, G.GAME.current_round.hands_left)
+			SMODS.calculate_effect({message = localize('k_reset'), colour = G.C.FILTER}, card)  -- resets when added to deck
+		end
+		
+	end,
 	
     calculate = function(self, card, context)
         if context.setting_blind and not card.getting_sliced then
-			card.ability.extra.success = false
-			local total_hands = G.GAME.current_round.hands_left
-			card.ability.extra.aim = math.random(1, total_hands)
-			if card.ability.extra.once == 0 then card.ability.extra.once = 1 end
-			return {
-                message = localize('k_reset'),
-                colour = G.C.FILTER,
-            }
+			if card.ability.extra.add_check then  -- skips first reset after added to deck
+				card.ability.extra.add_check = false  -- useful for when the card is added by other means than the shop, eg. Riff-Raff.
+			else
+				card.ability.extra.success = false
+				card.ability.extra.aim = math.random(1, G.GAME.current_round.hands_left)
+				return {
+					message = localize('k_reset'),
+					colour = G.C.FILTER,
+				}
+			end
 		end
 		if context.end_of_round and not context.blueprint and not context.repetition and not context.other_card then
 			if card.ability.extra.aim == G.GAME.current_round.hands_played then
 				card.ability.extra.success = true
+				card.ability.extra.add_check = false  -- prevents skipping next reset if added mid-round
 				return {
 					message = localize('buf_hit'),
 					colour = G.C.GREEN,
